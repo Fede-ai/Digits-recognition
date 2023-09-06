82049928e7953a4b6d66dd49726a7295e9ea7eac
Federico Lucchi <federico.lucchi07@gmail.com>
2023-09-07 00:43:52 +0200

    implemented controll for trolls

--- Process-images/main.cpp
+++ Process-images/main.cpp
@@ -12,20 +12,42 @@ int main()
 	std::fstream file;
 	file.open("C:/Users/feder/Downloads/butterfly.txt", std::ios::out | std::ios::trunc);
 
+	//format the file and eliminate troll ones
 	for (int i = 0; i < 16000; i++)
 	{
 		std::string line;
 		getline(raw, line);
-		while (line.at(0) != '[')
+		bool valid = true;
+		int apost = 0;
+		for (int pos = 0; pos < line.size(); pos++)
 		{
-			line.erase(line.begin());
+			if (line.at(pos) == '"')
+			{
+				apost++;
+				if (apost == 14)
+				{
+					valid = (line.at(pos + 2) == 't');
+					break;
+				}
+			}
 		}
-		line.erase(line.begin());
-		line.erase(line.end()-1);
-		line.erase(line.end()-1);
-		line.append(";");
+		if (valid)
+		{
+			while (line.at(0) != '[')
+			{
+				line.erase(line.begin());
+			}
+			line.erase(line.begin());
+			line.erase(line.end()-1);
+			line.erase(line.end()-1);
+			line.append(";");
 
-		file << line + "\n";
+			file << line + "\n";
+		}
+		else
+		{
+			i--;
+		}
 	}
 
 	raw.close();
@@ -36,33 +58,40 @@ int main()
 	{
 		sf::Image image;
 		image.create(256, 256, sf::Color::White);
+		char div = ' ';
 
-		std::vector<int> strokeX;
-		std::vector<int> strokeY;
-		int num;
-		char div;
-		file >> div;
-		file >> div;
+		while (div != ';')
+		{
+			std::vector<int> strokeX;
+			std::vector<int> strokeY;
+			int num;
+			file >> div;
+			file >> div;
 		
-		sf::Vector2i value;
-		while (div != ']')
-		{
-			file >> num;
-			strokeX.push_back(num);
+			sf::Vector2i value;
+			while (div != ']')
+			{
+				file >> num;
+				strokeX.push_back(num);
+				file >> div;
+			}
+			file >> div; 
 			file >> div;
-		}
-		file >> div; 
-		file >> div;
 
-		while (div != ']')
-		{
-			file >> num;
-			strokeY.push_back(num);
+			while (div != ']')
+			{
+				file >> num;
+				strokeY.push_back(num);
+				file >> div;
+			}
+
+			file >> div;		
 			file >> div;
+			
+			simulateStroke(image, strokeX, strokeY);
 		}
 
-		simulateStroke(image, strokeX, strokeY);
-		image.saveToFile("image.png");
+		image.saveToFile("C:/Users/feder/Downloads/but/image" + std::to_string(i) + ".png");
 	}
 
 	file.close();
@@ -71,8 +100,18 @@ int main()
 
 void simulateStroke(sf::Image& image, std::vector<int> strokeX, std::vector<int> strokeY)
 {
-	for (int i = 0; i < strokeX.size(); i++)
+	for (int i = 0; i < strokeX.size() - 1; i++)
 	{
-		image.setPixel(strokeX[i], strokeY[i], sf::Color::Black);
+		float num;
+		if (abs(strokeX[i] - strokeX[i + 1]) > abs(strokeY[i] - strokeY[i + 1]))
+			num = strokeX[i] - strokeX[i + 1];
+		else
+			num = strokeY[i] - strokeY[i + 1];
+
+		for (int a = 0; a < abs(num); a++)
+		{
+			float per = float(a) / abs(num);
+			image.setPixel(strokeX[i] + (strokeX[i + 1] - strokeX[i]) * per, strokeY[i] + (strokeY[i+1] - strokeY[i]) * per, sf::Color::Black);
+		}
 	}
 }
